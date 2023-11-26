67d5da6645ec9e138ef3bfcec3448af926fd002a
abinshihab <22618390+abinshihab@users.noreply.github.com>
2023-11-26 06:24:41 +0000

    Update personal information in the profile

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Ahmed Bin Shehab",
+  title: "IT Support Engineer",
+  email: "a.shihab@hotmail.com",
+  gitHub: "https://github.com/abinshihab",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/ahmedshihab2023/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
